177dc0ad88415e2129b4cbdfdef27228fecfca30
DenisMiso4enko <denrrmisochenko17@gmail.com>
2023-06-15 14:28:12 +0300

    push

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+const router = express.Router();
+dotenv.config();
+
+const app = express()
+const PORT = process.env.PORT
+app.use(router)
+
+router.get("/", (req, res, next) => {
+	res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+	console.log(`port start ${PORT}`)
+})
+
